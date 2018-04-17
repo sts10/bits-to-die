db9e2a9c5412981c7090ef398d7d6be2b6564554
sts10 <sschlinkert@gmail.com>
2018-04-16 23:24:34 -0400

    converts bits to die

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
+use std::io;
 fn main() {
-    let number_of_words: f64 = 7776 as f64;
+    println!("Enter number of bits to be converted to die");
+    let bits = get_float_from_input() as f64;
 
-    println!("log base 2 of 5 is {}", log_base_2(5 as f64));
-    println!("log base 2 of 6 is {}", log_base_2(6 as f64));
-    println!("H of 7776 is {}", log_base_2(number_of_words));
-    println!("H of 7776^3 is {}", log_base_2(7776_i64.pow(3) as f64));
     println!(
-        "H of compromised 3 is {}",
-        log_base_2((7776_i64.pow(3) * 5) as f64)
+        "{} bits is equivalent to {} die",
+        bits,
+        bits_to_die(bits as f64)
     );
-    println!("Hello, world!");
 }
 
-fn log_base_2(word_count: f64) -> f64 {
-    let base_2: f64 = 2.0;
-    return (word_count.ln() / base_2.ln()) as f64;
+fn log_base_6(n: f64) -> f64 {
+    let six_as_float: f64 = 6.0;
+    return (n.ln() / six_as_float.ln()) as f64;
+}
+
+fn bits_to_die(bits: f64) -> f64 {
+    let possibilities: f64 = 2_f64.powf(bits) as f64;
+    return log_base_6(possibilities);
+}
+
+fn get_float_from_input() -> f64 {
+    let reader: io::Stdin = io::stdin();
+    let mut input_text: String = String::new();
+    let result: Result<usize, io::Error> = reader.read_line(&mut input_text);
+    if result.is_err() {
+        println!("failed to read from stdin");
+    }
+    let trimmed: &str = input_text.trim();
+    let option: Option<f64> = trimmed.parse::<f64>().ok();
+    match option {
+        Some(i) => return i,
+        None => return 0.0,
+    };
 }
